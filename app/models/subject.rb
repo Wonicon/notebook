fd2c437792a1be4b613805a370b8f6f89d549eb8
Wang Huizhe <conciselove@outlook.com>
2017-10-06 12:39:58 +0900

    add back-references

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,6 @@
 class Subject < ApplicationRecord
   belongs_to :category
+  has_many :posts, dependent: :destroy
+  has_many :journals, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :subject
+  has_many :task_items, dependent: :destroy
 end
