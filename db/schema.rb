73fa58ea579114d664037245e84c51d586d4bb12
Wang Huizhe <conciselove@outlook.com>
2017-10-07 18:21:45 +0800

    Add due date to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006033303) do
+ActiveRecord::Schema.define(version: 20171007084701) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20171006033303) do
     t.integer "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "due_date"
     t.index ["subject_id"], name: "index_tasks_on_subject_id"
   end
 
